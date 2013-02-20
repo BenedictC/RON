17f546dc2c7dbac88ec2b8fee52e79f76a7c92bd
Benedict Cohen <ben@benedictcohen.co.uk>
2013-02-20 18:17:27 +0000

    - Basic implementation of main

--- ron/main.m
+++ ron/main.m
@@ -15,13 +15,31 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool 
     {
+        //Read
+        NSString *ronPath = [[[NSProcessInfo processInfo] arguments] objectAtIndex:1];
+        
+        NSInputStream *inStream = [NSInputStream inputStreamWithFileAtPath:ronPath];
+        [inStream open];
+        NSError *error;
+        id objects = [EMKRONSerialization RONObjectWithStream:inStream options:0 error:&error];
+        if (objects == nil) {
+            fprintf(stderr, "%s", [[error localizedDescription] UTF8String]);
+        }
+        
+        //Write
+        NSData *json = [NSJSONSerialization dataWithJSONObject:objects options:0 error:&error];
+        if (json == nil) {
+            fprintf(stderr, "%s", [[error localizedDescription] UTF8String]);
+        }
+
+        fprintf(stdout, "%s", [[[NSString alloc] initWithData:json encoding:NSUTF8StringEncoding] UTF8String]);
+        
         //TODO: 
         // Implicition conversions
         // JSON -> RON
         // RON  -> JSON
         //
         // - Add switches for explict conversion
-        // - read input and write output        
     }
     return 0;
 }
